6cf27be7d8b6f20ece14115c479b44fb794a8ca7
배희열 <@DESKTOP-3AVMOT2>
2022-05-25 00:26:56 +0900

    ds

--- chap06_example/src/com/yedam/homework/Main.java
+++ chap06_example/src/com/yedam/homework/Main.java
@@ -7,10 +7,31 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		boolean run = true;
-		int num =sc.nextInt();
-		while(run) {
-			
+		ProductSystem pds = new ProductSystem();
+		while (run) {
+			System.out.println("1.상품 수 입력 2.상품 및 가격 입력 3.가격 출력 4.분석 5.종료");
+			int num = sc.nextInt();
+
+			if (num == 1) {
+				System.out.println("상품 수 입력 : ");
+				int num1 = sc.nextInt();
+				pds.setListSize(num1);
+
+			} else if (num == 2) {
+				String pdName = sc.next();
+				int pdPrice = sc.nextInt();
+				pds.putProduct(pdName, pdPrice);
+
+			} else if (num == 3) {
+				pds.printList();
+
+			} else if (num == 4) {
+				pds.analysis();
+
+			} else if (num == 5) {
+				System.out.println("종료");
+				run = false;
+			}
 		}
 	}
-
 }
--- chap06_example/src/com/yedam/homework/ProductSystem.java
+++ chap06_example/src/com/yedam/homework/ProductSystem.java
@@ -3,6 +3,9 @@ package com.yedam.homework;
 public class ProductSystem {
 	private Product[] list;
 	private int index;
+	private int max;
+	private int temp;
+	private int total;
 
 	public ProductSystem() {
 		index = -1;
@@ -11,25 +14,49 @@ public class ProductSystem {
 	// 1.상품 수 입력
 	public void setListSize(int size) {
 		list = new Product[size];
+		temp = size;
 	}
 
 	// 2.상품 및 가격 입력
 	public void putProduct(String name, int price) {
 		Product product = new Product();
-		product.setName(name);
-		product.setPrice(price);
-
-		list[++index] = product;
+		{
+			product.setName(name);
+			product.setPrice(price);
+			list[++index] = product;
+		}
 	}
 
 	// 3.제품별 가격 출력
 	public void printList() {
 		for (int i = 0; i <= index; i++) {
 			Product product = list[i];
-			//System.out.println(list[i].getPrice());
+			// System.out.println(list[i].getPrice());
+			System.out.printf("%d : ", i + 1);
 			list[i].showInfo();
 		}
 	}
 
 	// 4.분석
+	public void analysis() {
+		Product product;
+		max = list[0].getPrice();
+
+		for (int i = 1; i <= index; i++) {
+			if (max < list[i].getPrice()) {
+				max = list[i].getPrice();
+			}
+		}
+
+		for (int i = 0; i <= index; i++) {
+			if (max != list[i].getPrice()) {
+				total += list[i].getPrice();
+			} else {
+				list[i].showInfo();
+			}
+		}
+		System.out.printf("%d\n", total);
+
+	}
+
 }
